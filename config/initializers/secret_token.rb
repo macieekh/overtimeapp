47af950315c2dc7aa116d1acb986c43a41b3b5e5
Maciek <maciej.harazinski@gmail.com>
2014-03-16 17:03:15 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Overtimeapp::Application.config.secret_key_base = '9985ed62e908334c4aab59b333c1b0868ca109dc97398c09a5297e3988db18778273a2c3941c9c9693b838a9416fe903312d062ae45172ab98c18e42c947ec5d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Overtimeapp::Application.config.session_store :cookie_store, key: '_overtimeapp_session'
